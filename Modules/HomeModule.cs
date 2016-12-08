af62b8a1d014c1d75bb6868b5722e794a0774d89
Ryan Mathisen and Ryan Peterson <ryan+ryan@>
2016-12-07 16:28:09 -0800

    add multiple views and routes to front end

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -10,7 +10,48 @@ namespace Food
     {
       Get["/"] = _ =>
       {
-        return View["index.cshtml"];
+        List<Cuisine> allCuisines = Cuisine.GetAll();
+        return View["index.cshtml", allCuisines];
+      };
+
+      Get["/cuisine-form"] = _ =>
+      {
+        return View["cuisine_form.cshtml"];
+      };
+
+      Get["/cuisine-restaurants/{id}"] = parameters =>{
+        Cuisine selectedCuisine = Cuisine.Find(parameters.id);
+        List<Restaurant> restaurantList = selectedCuisine.GetRestaurants();
+        return View["cuisine_restaurants.cshtml", restaurantList];
+      };
+
+      Get["/restaurant-form"] = _ =>
+      {
+        List<Cuisine> newList = Cuisine.GetAll();
+        return View["restaurant_form.cshtml", newList];
+      };
+
+      Post["/add-cuisine"] = _ =>
+      {
+        Cuisine newCuisine = new Cuisine(Request.Form["cuisineform"]){};
+        newCuisine.Save();
+        List<Cuisine> newList = Cuisine.GetAll();
+        return View["index.cshtml", newList];
+      };
+
+      Post["/add-restaurant"] = _ =>
+      {
+        Restaurant newRestaurant = new Restaurant(Request.Form["restform"], Request.Form["cuisine-id"]){};
+        newRestaurant.Save();
+
+        List<Cuisine> allCuisines = Cuisine.GetAll();
+        return View["index.cshtml", allCuisines];
+      };
+
+      Get["/home"] = _ =>
+      {
+        List<Cuisine> allCuisines = Cuisine.GetAll();
+        return View["index.cshtml", allCuisines];
       };
     }
   }
